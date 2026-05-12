2374a26b7d9953c96d3b264aa1ee3ffa344f7c5d
Daniel Benavides <dasebesi2001@gmail.com>
2026-05-12 21:22:59 +0200

    creamos ejemplo de errores para ir viendo posibilidades de mejora

---
+++ servicios_excepeciones.py
@@ -0,0 +1,12 @@
+class ProyectoError(Exception):
+    """clase base para las excepciones en este módulo."""
+    pass    
+
+
+class elemento_no_encontrado(ProyectoError):
+    """se lanza cuando no se encuentra un elemento específico en la base de datos o en la colección."""
+    pass
+
+class ValidacionError(ProyectoError):
+    """se lanza cuando se produce un error de validación en los datos de entrada."""
+    pass
